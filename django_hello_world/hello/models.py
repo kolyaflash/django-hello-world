afdfabd319bec1121aaa48787acbafbba53c38ff
Nikolay Baluk <kolyaflash@gmail.com>
2014-03-24 21:43:30 +0200

    [ticket:1] Turn contacts field to Contacts model.

--- django_hello_world/hello/models.py
+++ django_hello_world/hello/models.py
@@ -5,5 +5,16 @@ class MyData(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birth_date = models.DateField()
-    bio = models.TextField()
-    contacts = models.TextField()
+    bio = models.TextField(null=True)
+    contacts_additional = models.TextField(null=True)
+
+
+class Contacts(models.Model):
+    CONTACT_TYPES = (
+        ('email', 'Email'),
+        ('skype', 'Skype'),
+        ('JID', 'Jabber'),
+    )
+    data = models.ForeignKey(MyData)
+    contact_type = models.CharField(max_length=100, choices=CONTACT_TYPES)
+    value = models.CharField(max_length=100)
