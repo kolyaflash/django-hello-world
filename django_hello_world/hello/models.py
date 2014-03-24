e478140af8f781a3e4c5e24fbb1224e3fe637e88
Nikolay Baluk <kolyaflash@gmail.com>
2014-03-24 21:15:25 +0200

    [ticket:1] Add MyData model and init migrations

--- django_hello_world/hello/models.py
+++ django_hello_world/hello/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class MyData(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    bio = models.TextField()
+    contacts = models.TextField()
