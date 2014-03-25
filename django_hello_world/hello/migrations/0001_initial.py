afa5e6c28b6f30333fb761524c59dc64ded3422c
Nikolay Baluk <kolyaflash@gmail.com>
2014-03-25 12:13:47 +0200

    [ticket:3] Add RequestLog model and middleware to log requests

--- django_hello_world/hello/migrations/0001_initial.py
+++ django_hello_world/hello/migrations/0001_initial.py
@@ -8,13 +8,109 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        # Adding model 'MyData'
+        db.create_table('hello_mydata', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('birth_date', self.gf('django.db.models.fields.DateField')()),
+            ('bio', self.gf('django.db.models.fields.TextField')(null=True)),
+            ('contacts_additional', self.gf('django.db.models.fields.TextField')(null=True)),
+        ))
+        db.send_create_signal('hello', ['MyData'])
+
+        # Adding model 'Contacts'
+        db.create_table('hello_contacts', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('data', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['hello.MyData'])),
+            ('contact_type', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('value', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal('hello', ['Contacts'])
+
+        # Adding model 'RequestLog'
+        db.create_table('hello_requestlog', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, on_delete=models.SET_NULL)),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('path', self.gf('django.db.models.fields.CharField')(max_length=500)),
+            ('method', self.gf('django.db.models.fields.CharField')(max_length=10)),
+        ))
+        db.send_create_signal('hello', ['RequestLog'])
+
 
     def backwards(self, orm):
-        pass
+        # Deleting model 'MyData'
+        db.delete_table('hello_mydata')
+
+        # Deleting model 'Contacts'
+        db.delete_table('hello_contacts')
+
+        # Deleting model 'RequestLog'
+        db.delete_table('hello_requestlog')
+
 
     models = {
-        
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'hello.contacts': {
+            'Meta': {'object_name': 'Contacts'},
+            'contact_type': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'data': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['hello.MyData']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'hello.mydata': {
+            'Meta': {'object_name': 'MyData'},
+            'bio': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+            'birth_date': ('django.db.models.fields.DateField', [], {}),
+            'contacts_additional': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'hello.requestlog': {
+            'Meta': {'ordering': "['-date']", 'object_name': 'RequestLog'},
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'method': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'path': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'on_delete': 'models.SET_NULL'})
+        }
     }
 
     complete_apps = ['hello']
--- django_hello_world/hello/models.py
+++ django_hello_world/hello/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class MyData(models.Model):
@@ -15,6 +16,31 @@ class Contacts(models.Model):
         ('skype', 'Skype'),
         ('JID', 'Jabber'),
     )
-    data = models.ForeignKey(MyData)
+    data = models.ForeignKey(MyData, on_delete=models.CASCADE)
     contact_type = models.CharField(max_length=100, choices=CONTACT_TYPES)
     value = models.CharField(max_length=100)
+
+
+class RequestLog(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    date = models.DateTimeField(auto_now_add=True)
+    path = models.CharField(max_length=500)
+    method = models.CharField(max_length=10)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __init__(self, *args, **kwargs):
+        """
+        Pass `_request` as kwarg for fill objects with data.
+        `_request` should be request instance.
+        """
+
+        request = kwargs.pop("_request", None)
+        if request:
+            if isinstance(request.user, User):
+                self.user = request.user
+            self.path = request.get_full_path()
+            self.method = request.method
+
+        super(RequestLog, self).__init__(*args, **kwargs)
