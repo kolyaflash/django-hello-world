2564c658dffd889704cfa7f8a13c91858538c560
Nikolay Baluk <kolyaflash@gmail.com>
2014-03-25 13:01:19 +0200

    [ticket:3] Add tests for requests

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -41,7 +41,7 @@ class CommonTest(TestCase):
         from django_hello_world.hello.models import MyData
 
         response = self.client.get(reverse('home'))
-        # profile = response.context["profile"]
+        profile = response.context["profile"]
         self.assertTrue('profile' in response.context)
         profile = response.context['profile']
         self.assertTrue(isinstance(profile, MyData))
@@ -53,7 +53,13 @@ class CommonTest(TestCase):
         from django_hello_world.hello.models import RequestLog
         response = self.client.get(reverse('home'))
         self.assertEqual(RequestLog.objects.all().count(), 1)
+        log = RequestLog.objects.all()[0]
+        self.assertEqual(log.method, 'GET')
+        self.assertEqual(log.path, reverse('home'))
+        self.assertIsNotNone(log.date)
+        self.assertIsNone(log.user)
 
     def test_request_page(self):
         response = self.client.get(reverse('requests'))
         self.assertTrue('requests' in response.context)
+        self.assertEqual(len(response.context['requests']), 1)
