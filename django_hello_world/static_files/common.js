9d15312262763e7e6434f3843f598a335f5b3a64
Nikolay Baluk <kolyaflash@gmail.com>
2014-03-31 12:55:23 +0300

    [ticket:6] Move js from template to file.

---
+++ django_hello_world/static_files/common.js
@@ -0,0 +1,34 @@
+function ajaxForm(formSelector) {
+    var defaultSelector = ".ajax-form";
+    if (!formSelector) {
+        formSelector = defaultSelector;
+    }
+    var form = jQuery(formSelector);
+    var stateHolder = jQuery(".form-state", form);
+
+    jQuery(document).on("change", '.ajax-form input, .ajax-form select, .ajax-form textarea', function (e) {
+        stateHolder.text("Changes not saved yet");
+    });
+
+    jQuery(formSelector).on("submit", function (e) {
+
+
+        var options = {
+            target: jQuery("fieldset", jQuery(this)),
+            beforeSubmit: function (formData, jqForm, options) {
+                jQuery("fieldset", jqForm).prop("disabled", true);
+                stateHolder.text("Submiting...");
+            },
+            success: function (responseText, statusText, xhr, $form) {
+                stateHolder.text("Changes saved!");
+                jQuery("fieldset", $form).prop("disabled", false);
+            },
+            error: function (responseText, statusText, xhr, $form) {
+                stateHolder.text("Changes not saved!");
+                jQuery("fieldset", $form).prop("disabled", false);
+            },
+        };
+        jQuery(this).ajaxSubmit(options);
+        return false;
+    });
+}
